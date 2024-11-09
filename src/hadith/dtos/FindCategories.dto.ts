6bfd4de69ae7d04494f72c16ffa011dc64156884
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2024-11-09 11:29:49 +0200

    update

--- src/hadith/dtos/FindCategories.dto.ts
+++ src/hadith/dtos/FindCategories.dto.ts
@@ -1,23 +1,29 @@
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
+import { ToNumber } from 'src/common/constant/decorator/ToNumber';
 
 export class FindCategoriesDto {
   static getQuery(query: FindCategoriesDto) {
-    const mongooseQuery = {};
+    const mongooseQuery = { $and: [] };
     if (query.ketab_id) {
-      mongooseQuery['ketab_id'] = query.ketab_id;
+      mongooseQuery.$and.push({ ketab_id: Number(query.ketab_id) });
     }
 
     if (query.fasl_id) {
-      mongooseQuery['fasl_id'] = query.fasl_id;
+      mongooseQuery.$and.push({ fasl_id: Number(query.fasl_id) });
     }
 
     return mongooseQuery;
   }
 
   @IsNumber()
+  @Type(() => Number)
+  @ToNumber()
   ketab_id: number;
 
   @IsOptional()
   @IsNumber()
+  @Type(() => Number)
+  @ToNumber()
   fasl_id?: number;
 }
